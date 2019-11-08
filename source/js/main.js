509d5e17aa991f740eef5ab358e5f58f21e3a1d6
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-11-08 17:27:12 +0500

    Сделал подвал

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,36 @@
 const headerButton = document.getElementById('header-button');
-console.dir(headerButton);
+
+
+const siteMap = {
+  toggleButton: document.getElementById('footer-toggle'),
+  siteMapList: document.querySelector('.site-map__list'),
+  siteMapListLabel: document.querySelector('.footer__toggle-label'),
+}
+
+const footerAdress = {
+  toggleButton: document.getElementById('adress-toggle'),
+  siteMapList: document.querySelector('.footer__adress--description'),
+  siteMapListLabel: document.querySelector('.adress-toggle--label'),
+}
 
 if(headerButton && document.documentElement.clientWidth < 768) {
   headerButton.textContent = "бесплатная консультация";
 };
+
+const onToggleButtonClick = ({toggleButton, siteMapList, siteMapListLabel}) => {
+  if (toggleButton.checked) {
+    siteMapList.style = 'display: block';
+    siteMapListLabel.style = 'height: 2px; overflow: hidden'
+  } else {
+    siteMapList.style = 'display: none';
+    siteMapListLabel.style = 'height: 17px'
+  }
+}
+
+siteMap.toggleButton.addEventListener('change', () => {
+  onToggleButtonClick(siteMap)
+});
+
+footerAdress.toggleButton.addEventListener('change', () => {
+  onToggleButtonClick(footerAdress)
+});
