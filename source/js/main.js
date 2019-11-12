5d45e60ed1870a72ce7092b980484086f6558c72
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-11-13 02:10:44 +0500

    Исправления по баглисту

--- source/js/main.js
+++ source/js/main.js
@@ -1,53 +1,93 @@
 var headerButton = document.getElementById('header-button');
-var callbackButton = document.querySelectorAll('.modal-button');
+var callbackButton = document.querySelector('.modal-button');
 var modalContainer = document.querySelector('.modal-form');
 var modalCloseButton = document.querySelector('.modal-form__close-button');
 
-var mainForm = document.querySelector('.main-form').querySelector('form');
-var mainName = mainForm.querySelector('[name=name]');
+var mainForm = document.querySelector('.main-form');
+var mainName = document.getElementById('main-form-name');
 var mainTel = mainForm.querySelector('[name=tel]');
 var mainTextArea = mainForm.querySelector('[name=comment]');
 
 var modalForm = document.querySelector('.modal-form');
 var form = modalForm.querySelector('form');
-var name = form.querySelector('[name=name]');
+var nameFooter = form.querySelector('[name=name-popup]');
 var tel = form.querySelector('[name=tel]');
 var textArea = form.querySelector('[name=comment]');
 
+var footerRollBar = document.querySelectorAll('.roll-bar');
+var footer = document.querySelector('.footer__wrapper');
+var footerToggleButton = document.querySelectorAll('.footer__toggle');
 
-var siteMap = {
-  toggleButton: document.getElementById('footer-toggle'),
-  siteMapList: document.querySelector('.site-map__list'),
-  siteMapListLabel: document.querySelector('.footer__toggle-label'),
-}
-
-var footerAdress = {
-  toggleButton: document.getElementById('adress-toggle'),
-  siteMapList: document.querySelector('.footer__adress-description'),
-  siteMapListLabel: document.querySelector('.adress-toggle-label'),
-}
+var footerFeedback = document.querySelector('.feedback');
 
 if(headerButton && document.documentElement.clientWidth < 768) {
   headerButton.textContent = "бесплатная консультация";
 };
 
-var onToggleButtonClick = function(toggleButton, siteMapList, siteMapListLabel) {
-  if (toggleButton.checked) {
-    siteMapList.style = 'display: block';
-    siteMapListLabel.style = 'height: 2px; overflow: hidden'
-  } else {
-    siteMapList.style = 'display: none';
-    siteMapListLabel.style = 'height: 17px'
-  }
-}
+headerButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  footerFeedback.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+})
 
-siteMap.toggleButton.addEventListener('change', function() {
-  onToggleButtonClick(siteMap.toggleButton, siteMap.siteMapList, siteMap.siteMapListLabel)
-});
+footerToggleButton.forEach(function(it) {
+  it.addEventListener('change', function(evt) {
+    evt.preventDefault();
+    footerRollBar.forEach(function(bar) {
+      if(bar.classList.contains('roll-bar--open')) {
+        bar.classList.remove('roll-bar--open');
+        // bar.classList.add('visually-hidden');
+      } else {
+        bar.classList.add('roll-bar--open');
+        // bar.classList.remove('visually-hidden');
+      }
+    })
+  })
+})
 
-footerAdress.toggleButton.addEventListener('change', function() {
-  onToggleButtonClick(footerAdress.toggleButton, footerAdress.siteMapList, footerAdress.siteMapListLabel)
-});
+// footer.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+
+//   if (evt.target.contains('footer__toggle-label')) {
+//     footerRollBar.forEach(function(it) {
+
+//     })
+//   }
+// })
+
+
+// var siteMap = {
+//   toggleButton: document.getElementById('footer-toggle'),
+//   siteMapList: document.querySelector('.site-map__list'),
+//   siteMapListLabel: document.querySelector('.footer__toggle-label'),
+// }
+
+// var footerAdress = {
+//   toggleButton: document.getElementById('adress-toggle'),
+//   siteMapList: document.querySelector('.footer__adress-description'),
+//   siteMapListLabel: document.querySelector('.adress-toggle-label'),
+// }
+
+// if(headerButton && document.documentElement.clientWidth < 768) {
+//   headerButton.textContent = "бесплатная консультация";
+// };
+
+// var onToggleButtonClick = function(toggleButton, siteMapList, siteMapListLabel) {
+//   if (toggleButton.checked) {
+//     siteMapList.style = 'display: block';
+//     siteMapListLabel.style = 'height: 2px; overflow: hidden'
+//   } else {
+//     siteMapList.style = 'display: none';
+//     siteMapListLabel.style = 'height: 17px'
+//   }
+// }
+
+// siteMap.toggleButton.addEventListener('change', function() {
+//   onToggleButtonClick(siteMap.toggleButton, siteMap.siteMapList, siteMap.siteMapListLabel)
+// });
+
+// footerAdress.toggleButton.addEventListener('change', function() {
+//   onToggleButtonClick(footerAdress.toggleButton, footerAdress.siteMapList, footerAdress.siteMapListLabel)
+// });
 
 var isStorageSupport = true;
 var isStorageEmail = true;
@@ -60,15 +100,15 @@ var onModalOpen = function (evt) {
     modalForm.classList.remove("modal-form--closed");
 
     if (storage && telStorage) {
-      name.value = storage;
+      nameFooter.value = storage;
       tel.value = emailStorage;
       textArea.focus();
     } else if(storage) {
-      name.value = storage;
+      nameFooter.value = storage;
       console.log(name);
       tel.focus();
     } else {
-      name.focus();
+      nameFooter.focus();
     }
   }
 };
@@ -85,9 +125,7 @@ try {
   isStorageEmail = false;
 }
 
-for (var i = 0; i < callbackButton.length; i++) {
-  callbackButton[i].addEventListener("click", onModalOpen);
-}
+callbackButton.addEventListener("click", onModalOpen);
 
 modalCloseButton.addEventListener('click', function(evt) {
   evt.preventDefault();
